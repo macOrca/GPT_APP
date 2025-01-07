8881454b285e57711f8dc806777a314a120f953b
macOrca <84633201+macOrca@users.noreply.github.com>
2025-01-08 06:42:02 +0900

    Update student_page.py

--- student_page.py
+++ student_page.py
@@ -51,8 +51,10 @@ def student_dashboard():
                 st.code(output_result)
                 st.write("フィードバック:")
                 st.write(feedback.feedback)
+                st.write(feedback)
                 user_profile = analyze_user_feedback(feedback, user_profile)
                 save_user_profile(st.session_state.user_id, user_profile)
+                st.write(user_profile)
                 fetch_user_profile(st.session_state.user_id)
         else:
             st.write("練習問題はありません。")
